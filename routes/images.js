cf93e77bb4af51da12a04c623a894830b28a194a
Matt Wyatt <mswyatt.td@gmail.com>
2024-03-22 11:11:31 -0700

    functioning post route

--- routes/images.js
+++ routes/images.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Image = require("../models/images");
 
+//GET ROUTES ------------------------------------------------------------------------------------
 //get all images || default sort by date (+++)
 router.get("/", async (req, res) => {
   let imagesList = await Image.find();
@@ -19,4 +20,23 @@ router.get("/:postTitle", async (req, res) => {
   let imagesList = await Image.find({ postTitle: req.params.postTitle });
   res.send(imagesList);
 });
+
+//POST ROUTES -------------------------------------------------------------------------------------
+//create a new post
+router.post("/newPost", async (req, res) => {
+  let newPost = new Image({
+    postTitle: req.body.postTitle,
+    datePosted: req.body.datePosted,
+    postedBy: req.body.postedBy,
+    tags: req.body.tags,
+    filePath: req.body.filePath,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+});
+
 module.exports = router;
